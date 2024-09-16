20f37e76a72b2c1ac03e524e9740909618e1ec20
cesar <cesarhernanruscica@gmail.com>
2024-09-16 09:35:03 -0300

    reactivaded the starCronjobs function

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use('/api/data', dataRoutes);
 
 app.use(errorHandler);
 
-//startCronJobs();
+startCronJobs();
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
