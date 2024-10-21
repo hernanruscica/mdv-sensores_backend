aab9b461c56681bd3e44c480b886f7f35c089630
cesar <cesarhernanruscica@gmail.com>
2024-10-21 18:41:40 -0300

    changes

--- src/services/CronService.js
+++ src/services/CronService.js
@@ -5,7 +5,7 @@ class CronService {
   startJobs() {
     cron.schedule('*/5 * * * * ', async () => { // Cada ... */1 * * * * = 1 minuto / */30 * * * * * = 30 segundos
       console.log('Verificando alarmas...');
-      await AlarmService.checkAlarms();
+      //await AlarmService.checkAlarms();
     });
   }
 }
