e831f9921f02a908a1c7d2260085fc1d245f4e56
cesar <cesarhernanruscica@gmail.com>
2024-10-29 10:23:39 -0300

    changed schedule time

--- src/services/CronService.js
+++ src/services/CronService.js
@@ -3,7 +3,7 @@ import AlarmService  from '../services/AlarmService.js';
 
 class CronService {
   startJobs() {
-    cron.schedule('*/30 * * * * * ', async () => { // Cada ... */1 * * * * = 1 minuto / */30 * * * * * = 30 segundos
+    cron.schedule('*/5 * * * * ', async () => { // Cada ... */1 * * * * = 1 minuto / */30 * * * * * = 30 segundos
       console.log('Verificando alarmas...');
       await AlarmService.checkAlarms();
     });
