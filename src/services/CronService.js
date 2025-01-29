601617ff540e13ebd8a41f1e02667ad886b80268
cesar <cesarhernanruscica@gmail.com>
2025-01-29 14:24:32 -0300

    AlarmService.checkAlarms()

--- src/services/CronService.js
+++ src/services/CronService.js
@@ -4,7 +4,7 @@ import AlarmService  from '../services/AlarmService.js';
 class CronService {
   startJobs() {
     cron.schedule('*/5 * * * * ', async () => { // Cada ... */1 * * * * = 1 minuto / */30 * * * * * = 30 segundos
-      console.log('Verificando alarmas...');
+      console.log('Verificando alarmas...s');
       await AlarmService.checkAlarms();
     });
   }
